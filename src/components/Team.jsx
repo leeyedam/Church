2089c6eaa6b1384598cdee7a0baf5fcac18224af
leeyedam <dldpeka0909@gmail.com>
2022-05-24 21:12:08 +0900

    first commit

---
+++ src/components/Team.jsx
@@ -0,0 +1,30 @@
+export const Team = (props) => {
+  return (
+    <div id='team' className='text-center'>
+      <div className='container'>
+        <div className='col-md-8 col-md-offset-2 section-title'>
+          <h2>교회 구성원</h2>
+          <p>
+            교회 구성원을 소개합니다.
+          </p>
+        </div>
+        <div id='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
+                  <div className='thumbnail'>
+                    {' '}
+                    <div className="team-img"><i className={d.img}/></div>
+                    <div className='caption'>
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : 'loading'}
+        </div>
+      </div>
+    </div>
+  )
+}
